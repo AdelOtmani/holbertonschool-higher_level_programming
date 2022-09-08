ffcf5e826a8c6817dfa9998c80a6ace671d3bcd7
AdelOtmani <5023@holbertonschool.com>
2022-09-08 20:37:50 +0200

    exo 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if (ord(i) >= 97 and ord(i) <= 122):
+            str[i] = str(i + 32)
+            i = i + 1
+        else:
+            i = i + 1
+    print(str)
