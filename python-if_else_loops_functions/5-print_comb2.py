1e4fbbd036da42e00e07cef4f2b1b26cf87a7a63
AdelOtmani <5023@holbertonschool.com>
2022-09-08 19:50:26 +0200

    exo 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+newline = '\n'
 for i in range(00, 100):
-    print("{:02d}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
+    print("{:02d}".format(i)+f"{', ' if i < 99 else newline}", end="")
     i = i + 1
