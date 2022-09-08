2ec0d7b10fd280f09d3812e941720b7cb5270881
AdelOtmani <5023@holbertonschool.com>
2022-09-08 22:04:35 +0200

    fix ord

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,5 @@
 def uppercase(str):
     for i in str:
         if (ord(i) >= 97 and ord(i) <= 122):
-            str[i] = chr(i + 32)
-            i = i + 1
-        else:
-            i = i + 1
+            str[i] = chr(ord(i) + 32)
     print("{:c}".format(str))
