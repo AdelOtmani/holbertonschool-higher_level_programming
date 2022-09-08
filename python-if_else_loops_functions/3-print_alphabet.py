20b4f9a7b5113f31a7643f79d096c3329f67cf80
AdelOtmani <5023@holbertonschool.com>
2022-09-08 18:50:00 +0200

    fix .format

--- python-if_else_loops_functions/3-print_alphabet.py
+++ python-if_else_loops_functions/3-print_alphabet.py
@@ -3,5 +3,5 @@ for i in range(97, 123):
     if (chr(i) == 'e' or chr(i) == 'q'):
         i = i + 1
     else:
-        print(f"{chr(i)}", end="")
+        print("{:c}".format(i), end='')
         i = i + 1
