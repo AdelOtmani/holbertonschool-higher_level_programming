8d572729ffae43fc2a075454bb394257b212552f
AdelOtmani <5023@holbertonschool.com>
2022-09-09 23:05:10 +0200

    exo 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    av = len(sys.argv) - 1
+    r = 0
+    for i in range(av):
+        r += int(sys.argv[i + 1])
+        print("{}".format(r))
