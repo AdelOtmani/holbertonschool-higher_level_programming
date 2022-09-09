bf18dad1ad68a0715be3feb3d8c6b215a5033ae3
AdelOtmani <5023@holbertonschool.com>
2022-09-09 23:32:40 +0200

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for d in range(10):
     for u in range(d + 1, 10):
         print("{}{}".format(d, u), end="")
         if d != 8 and u != 9:
-            print(", ")
+            print(", ", end="")
