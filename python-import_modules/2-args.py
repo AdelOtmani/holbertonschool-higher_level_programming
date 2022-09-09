376f16c93274353e4e822d3c6d3c820b294fa028
AdelOtmani <5023@holbertonschool.com>
2022-09-09 22:46:55 +0200

    exo 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    av = len(sys.argv)
+
+    if av == 0:
+        print("0 argument.")
+    elif av == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(av))
+    for j in range(1, av):
+        print("{}: {:s}".format(j, av))
