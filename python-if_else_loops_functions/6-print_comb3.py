21be62b9193b21b5fe933d59d7ec5f8c909f7efc
AdelOtmani <5023@holbertonschool.com>
2022-09-09 23:30:19 +0200

    exo 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+newline = '\n'
+for d in range(10):
+    for u in range(d + 1, 10):
+        print("{}{}".format(d, u), end="")
+        if d != 8 and u != 9:
+            print(", ")
