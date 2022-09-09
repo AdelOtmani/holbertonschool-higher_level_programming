17b6afcd4ee477d16670612293835aeae749596f
AdelOtmani <5023@holbertonschool.com>
2022-09-09 22:52:00 +0200

    fix code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     elif av == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(av-1))
+        print("{} arguments:".format(av))
     for j in range(1, av):
-        print("{}: {:s}".format(j, av[j]))
+        print("{}: {:s}".format(j, sys.argv[j]))
