5ba8848ccfcf1c9dbf61565ef8ae477ab62bdd8b
AdelOtmani <5023@holbertonschool.com>
2022-09-12 11:17:12 +0200

    exo 3 condition None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = 0
-    if None:
-        return []
+    if my_list is None:
+        print("", end="")
     else:
-        while i < len(my_list):
+        while i < (len(my_list)):
             r = (len(my_list) - 1) - i
             print("{:d}".format(my_list[r]))
             i = i + 1
