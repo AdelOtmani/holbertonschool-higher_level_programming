f37445ab7057bcb5ca76bebe9f4f40dc8e75198f
AdelOtmani <5023@holbertonschool.com>
2022-09-13 19:56:07 +0200

    exo 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,4 +3,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, ) * (2 - len(tuple_a))
     tuple_b += (0, ) * (2 - len(tuple_b))
     r = (map(lambda a, b: a + b, tuple_a, tuple_b))
-    return (tuple(r))
+    return (tuple(r)[:2])
