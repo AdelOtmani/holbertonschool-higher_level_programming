b06b1f83054fd536f7fe243826570d5eec9d05fb
AdelOtmani <5023@holbertonschool.com>
2022-09-13 20:04:00 +0200

    exo 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     n = len(sentence)
-    m = sentence[0]
-    t = (n, m)
-    return (t)
+    if sentence is None:
+        return
+    else:
+        m = sentence[0]
+        t = (n, m)
+        return (t)
