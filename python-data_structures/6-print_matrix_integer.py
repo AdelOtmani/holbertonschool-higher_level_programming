5c053178a69b60f6bd090453629089a237b740c6
AdelOtmani <5023@holbertonschool.com>
2022-09-13 19:00:53 +0200

    exo 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        j = " ".join([x.__str__() for x in i])
-        print("{}".format(j))
+        j = " ".join(['{:d}'.format(x) for x in i])
+        print(j)
