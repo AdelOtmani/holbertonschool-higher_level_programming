f6364595d235be9bca57c4cba1209d663ce4b6b4
AdelOtmani <5023@holbertonschool.com>
2022-09-14 15:13:26 +0200

    exo 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return (my_string.translate({ord(i): None})for i in my_string)
+    if my_string:
+        my_string = my_string.translate({ord(i): None for i in "Cc"})
+        return my_string
+    return my_string
