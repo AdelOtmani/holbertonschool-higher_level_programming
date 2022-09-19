dcefc438f1835fb805fe78b93562c97be6cd021d
AdelOtmani <5023@holbertonschool.com>
2022-09-19 12:28:20 +0200

    exo 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a Square class   """
+
+
+class Square(object):
+    """private instance
+"""
+
+    def __init__(self, size=0):
+        """init size"""
+        self.__size = size
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
