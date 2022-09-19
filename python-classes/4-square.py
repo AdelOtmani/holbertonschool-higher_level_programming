534e0692cae642a0bb8170600ab5bd008c27b000
AdelOtmani <5023@holbertonschool.com>
2022-09-19 12:51:43 +0200

    exo 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square(object):
 
         if (type(value) != int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
