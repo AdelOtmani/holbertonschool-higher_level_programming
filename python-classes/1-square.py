b263dd18887586ac3e62dfe1a43c1153732d6fb0
AdelOtmani <5023@holbertonschool.com>
2022-09-19 12:18:58 +0200

    exo 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a Square class   """
+
+
+class Square(object):
+    """private instance
+"""
+
+    def __init__(self, size):
+        """init class"""
+        self.__size = size
