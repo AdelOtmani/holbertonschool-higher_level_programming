eb64390b9b7116ab2bee342cd964507555bf8e0e
AdelOtmani <5023@holbertonschool.com>
2022-09-19 12:33:18 +0200

    exo 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a Square class   """
+
+
+class Square(object):
+    """private instance
+"""
+
+    def __init__(self, size=0):
+        """init size"""
+        self.__size = size
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """return current area"""
+
+        return self._size**2
