d7aa58f23c350e369e60b2cd52d14c1ba77afa9c
AdelOtmani <5023@holbertonschool.com>
2022-09-22 17:54:26 +0200

    test and exo 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """say my name function"""
 
+
 def print_square(size):
+    """ print square of "#"
+    """
+
     if type(size) != int or (type(size) == float and size == 0):
         raise TypeError("size must be an integer")
     if size < 0:
