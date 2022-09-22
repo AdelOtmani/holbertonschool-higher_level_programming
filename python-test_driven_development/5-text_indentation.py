3f70a22cc259cad19ab7ac9fb25f6d09cd6250bc
AdelOtmani <5023@holbertonschool.com>
2022-09-22 22:16:58 +0200

    test and exo 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" function identation string"""
+
+
+def text_indentation(text):
+    """ function that prints a text with 2 new lines after
+        each of these characters: ., ? and : """
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for letter in text:
+        if letter == "." or letter == "?" or letter == ":":
+            print(letter)
+            print("")
+        else:
+            print(letter, end="")
