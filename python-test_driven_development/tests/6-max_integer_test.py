4f531ee48271bf50ca5c59bb8c91ebe02b46896e
AdelOtmani <5023@holbertonschool.com>
2022-09-23 17:38:43 +0200

    exo 6 and unitest for 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """unittest class for max_integer"""
+
+    def onlypos_valuetest(self):
+        self.assertEqual(max_integer([1, 12, 9]), 12)
+
+    def onlyneg_value_test(self):
+        self.assertEqual(max_integer([-1, -12, -9]), -1)
+
+    def oneneg_test(self):
+        self.assertEqual(max_integer([1, 6, 500, -1, 300]), 500)
+
+    def single_test(self):
+        self.assertEqual(max_integer([22]), 22)
+
+    def floatint_test(self):
+        self.assertEqual(max_integer([5, 6, 3.2, 9.8]), 9.8)
+
+    def emptylist_test(self):
+        self.assertEqual(max_integer([]), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
