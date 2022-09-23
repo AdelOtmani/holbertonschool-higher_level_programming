782c1b55c96d2dff908f75e24d057befa22a7294
AdelOtmani <5023@holbertonschool.com>
2022-09-23 18:29:47 +0200

    exo 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,20 +8,20 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """unittest class for max_integer"""
 
-    def onlypos_value_test(self):
+    def test_onlypos_value(self):
         self.assertEqual(max_integer([1, 12, 9]), 12)
 
-    def onlyneg_value_test(self):
+    def test_onlyneg_value(self):
         self.assertEqual(max_integer([-1, -12, -9]), -1)
 
-    def oneneg_test(self):
+    def test_oneneg(self):
         self.assertEqual(max_integer([1, 6, 300, -1, 500]), 500)
 
-    def single_test(self):
+    def test_single(self):
         self.assertEqual(max_integer([22]), 22)
 
-    def floatint_test(self):
+    def test_floatint(self):
         self.assertEqual(max_integer([5, 6, 3.2, 9.8]), 9.8)
 
-    def emptylist_test(self):
+    def test_emptylist(self):
         self.assertEqual(max_integer([]), None)
