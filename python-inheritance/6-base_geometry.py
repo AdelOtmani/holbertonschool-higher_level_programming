80787b9e314f07cb8f991ad7b6462dd382f209b8
AdelOtmani <5023@holbertonschool.com>
2022-09-28 16:04:16 +0200

    exo 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class   """
+
+
+class BaseGeometry:
+    """ Public instance method: def area(self):
+
+"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
