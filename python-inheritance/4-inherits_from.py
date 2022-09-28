3db6baddaa112b34ad55a7e48bf3d504f6a6d191
AdelOtmani <5023@holbertonschool.com>
2022-09-28 15:46:16 +0200

    exo 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """ function that returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class ;
     otherwise False."""
-    return not isinstance(not type(obj), a_class)
+    return isinstance(not type(obj), a_class)
