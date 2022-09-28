c13f64de6e16d3940417d55843ed623b79cf6f35
AdelOtmani <5023@holbertonschool.com>
2022-09-28 16:32:37 +0200

    exo 7 and test

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,14 +6,11 @@ class BaseGeometry:
     """ Public instance method: def area(self):
 
 """
-    def __init__(self, name):
-        self.name = name
-
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
