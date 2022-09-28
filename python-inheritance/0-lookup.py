61f170ab6612e21b399f3bd8aaecc7c68de6520b
AdelOtmani <5023@holbertonschool.com>
2022-09-28 11:17:10 +0200

    exo 0 documented

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" fonction to look up object's attribut"""
+
+
 def lookup(obj):
+    """return list of attributs"""
     att = dir(obj)
     if hasattr(obj, '__class__'):
         return att
