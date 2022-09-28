600cdc259a79065d87a828997eb8f05971f2a011
AdelOtmani <5023@holbertonschool.com>
2022-09-28 17:27:42 +0200

    exo 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class   """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Public instance method: def area(self):
+
+"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
