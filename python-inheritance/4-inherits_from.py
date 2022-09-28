2aa981d4ace252d9f667f9e6369d5b9f4c1b223c
AdelOtmani <5023@holbertonschool.com>
2022-09-28 15:37:17 +0200

    exo 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""exo 3 project holberton"""
+
+
+def inherits_from(obj, a_class):
+    """ function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class ;
+    otherwise False."""
+    return not isinstance(type(obj), a_class)
