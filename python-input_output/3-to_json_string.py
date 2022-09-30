765084dbba5b190426bea916776073bd2ad71b81
AdelOtmani <5023@holbertonschool.com>
2022-09-30 21:31:02 +0200

    exo 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exercice 3 project Holberton Adel Otmani
+
+  """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """3. To JSON string
+
+    Args:
+        my_obj (_type_): type of object
+    Returns:
+            the JSON representation of an object (string):
+    """
+    return (json.dump(my_obj))
