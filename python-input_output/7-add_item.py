bade2cd0df19d73a7bae560102d47a058a5706f5
AdelOtmani <5023@holbertonschool.com>
2022-09-30 22:33:09 +0200

    exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,13 +2,15 @@
 """ Exercice 7 projet holberton Adel Otmani.
 
 """
+from os import path
 import sys
 import json
-from os import path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if not path.exists("add_item.json"):
+if path.exists("add_item.json"):
     with open("add_item.json", 'w', encoding="utf-8") as Myfile:
         Myfile.write(json.dumps([]))
 else:
