137b28120e4aedf6c64f37bae697765d1d080ce1
AdelOtmani <5023@holbertonschool.com>
2022-09-30 23:47:35 +0200

    exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,13 @@
 """
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        myList = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        myList = []
+open("add_item.json", "a")
+try:
+    myList = load_from_json_file("add_item.json")
+except ValueError:
+    myList = []
 
-    myList.extend(sys.argv[2:])
-    save_to_json_file(myList, "add_item.json")
+save_to_json_file(myList + sys.argv[1:], "add_item.json")
