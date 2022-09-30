7cea87fe02c5222795749d9dcb4a29b6036e4b8a
AdelOtmani <5023@holbertonschool.com>
2022-09-30 23:37:09 +0200

    exoc 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,13 +4,14 @@
 import sys
 import json
 from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = (
+        __import__('6-load_from_json_file').load_from_json_file)
 
-if not path.exists("add_item.json"):
-    with open("add_item.json", 'w', encoding="utf-8") as Myfile:
-        Myfile.write(json.dumps())
-
-myList = load_from_json_file("add_item.json")
-myList.extend(sys.argv[1:])
-save_to_json_file(myList, "add_item.json")
+    try:
+        myList = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        myList = []
+    myList.extend(sys.argv[1:])
+    save_to_json_file(myList, "add_item.json")
