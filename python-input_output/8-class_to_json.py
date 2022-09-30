8ac9c31613aa77a5eb87244855be3905424054ce
AdelOtmani <5023@holbertonschool.com>
2022-09-30 23:01:05 +0200

    exo 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Exercice 8 project holberton Adel Otmani
+"""
+
+
+def class_to_json(obj):
+    """8. Class to JSON
+
+    Args:
+        obj (_type_): is an instance of a Class
+    """
+    return obj.__dict__
