6db3f998b86e7219080cab976110e9595510823c
AdelOtmani <5023@holbertonschool.com>
2022-09-30 19:48:46 +0200

    exo 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Exercice 0 project Holberton Adel Otmani
+
+  """
+
 
 def read_file(filename=""):
-    """ read file """
+    """functun read file
+    Args:
+        filename (str): ."".
+    """
 
     with open("my_file_0.txt", encoding="utf-8") as Myfile:
         Myfile.read
