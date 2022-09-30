895fc6fd54640d5afb52b11af97a2301e6dcb994
AdelOtmani <5023@holbertonschool.com>
2022-09-30 23:20:10 +0200

    exo 7  and chmod

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,15 +4,13 @@
 import sys
 import json
 from os import path
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = (
-        __import__('6-load_from_json_file').load_from_json_file)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    if not path.exists("add_item.json"):
-        with open("add_item.json", 'w', encoding="utf-8") as Myfile:
-            Myfile.write(json.dumps([]))
+if not path.exists("add_item.json"):
+    with open("add_item.json", 'w', encoding="utf-8") as Myfile:
+        Myfile.write(json.dumps([]))
 
-    myList = load_from_json_file("add_item.json")
-    myList.extend(sys.argv[1:])
-    save_to_json_file(myList, "add_item.json")
+myList = load_from_json_file("add_item.json")0
+myList.extend(sys.argv[1:])
+save_to_json_file(myList, "add_item.json")
