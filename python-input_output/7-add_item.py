9344db765f0b23127c658b161ba92acd8453e12e
AdelOtmani <5023@holbertonschool.com>
2022-09-30 22:26:40 +0200

    exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,10 +3,15 @@
 
 """
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import json
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-obj = []
-obj = load_from_json_file("add_item.json")
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, "add_item.json")
+if not path.exists("add_item.json"):
+    with open("add_item.json", 'w', encoding="utf-8") as Myfile:
+        Myfile.write(json.dumps([]))
+
+myList = load_from_json_file("add_item.json")
+myList.extend(sys.argv[1:])
+save_to_json_file(myList, "add_item.json")
