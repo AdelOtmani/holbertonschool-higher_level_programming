16ba5ec2b96da4a35f9c80d3f213eae2840981f4
AdelOtmani <5023@holbertonschool.com>
2022-09-30 23:38:19 +0200

    exoc 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,6 @@
 """Exercice 7 project holberton Adel Otmani
 """
 import sys
-import json
-from os import path
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = (
