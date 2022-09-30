ec667de1032552fe4d2e01e3059fb7fad9d9af6b
AdelOtmani <5023@holbertonschool.com>
2022-09-30 22:17:29 +0200

    exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 """
         Exercice 7 project Holberton Adel Otmani
-
-
 """
 import sys
-"""
-    7. Load, add, save
-    """
-
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
