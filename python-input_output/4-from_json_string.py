1b00a5693e363216574a292562144ad917c056ed
AdelOtmani <5023@holbertonschool.com>
2022-09-30 21:36:54 +0200

    exo 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Exercice 4 project Holberton Adel Otmani
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """4. From JSON string to Object
+
+    Args:
+        my_str (_type_):
+    """
+    return (json.loads(my_str))
