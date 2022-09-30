948edc7c5cfe1b086a7e61eab247294e7e7dd5f0
AdelOtmani <5023@holbertonschool.com>
2022-09-30 22:35:59 +0200

    exo 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,9 +5,7 @@
 from os import path
 import sys
 import json
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists("add_item.json"):
