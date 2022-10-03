19c917f123114acc42d0bc1cd26367299df5d365
AdelOtmani <5023@holbertonschool.com>
2022-10-03 19:37:55 +0200

    Exercise 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Rectangle Class
+    """
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """create class Rectangle that inherits from Base.
+    Args:
+        Base (class):
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super(id).__init__(self, id=None)
