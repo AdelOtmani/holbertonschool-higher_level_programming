77f93be34c8a4be85473e3da735d8bc22df6b80d
AdelOtmani <5023@holbertonschool.com>
2022-10-04 12:55:17 +0200

    try property

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,10 +10,25 @@ class Rectangle(Base):
         Base (class):
     """
 
-    @property
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id=None)
+
+    @property
+    def __widht(self, value):
+        return self.__widht
+
+    @property
+    def __height(self, value):
+        return self.__height
+
+    @property
+    def __x(self, value):
+        return self.__x
+
+    @property
+    def __y(self, value):
+        return self.__y
