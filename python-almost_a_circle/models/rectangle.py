60f3b57cb5f67977afed7da6ea1109e25d4543b4
AdelOtmani <5023@holbertonschool.com>
2022-10-04 12:42:39 +0200

    try property

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,9 +9,11 @@ class Rectangle(Base):
     Args:
         Base (class):
     """
+
+    @property
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super(id).__init__(self, id=None)
+        super().__init__(id=None)
