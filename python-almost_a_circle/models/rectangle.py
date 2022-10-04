8b7ac181254fc919f5acfa7f121365f9654f4154
AdelOtmani <5023@holbertonschool.com>
2022-10-04 16:39:10 +0200

    exercice 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,7 @@ class Rectangle(Base):
             for j in range(self.width):
                 print(c, end="")
             print("")
+
+    def __str__(self):
+        return (f"[Rectangle] {self.id} {self.x}/{self.y}"
+                f" - {self.width}/{self.height}")
