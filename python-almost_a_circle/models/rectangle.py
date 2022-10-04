f0706bcbe2299f058b0f91f7413f519c50a25ec5
AdelOtmani <5023@holbertonschool.com>
2022-10-04 14:58:51 +0200

    correct raise error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Rectangle Class
     """
-from turtle import heading, width
 from models.base import Base
 
 
@@ -25,9 +24,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(self.__width))
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError(f"{self.__width} must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -37,9 +36,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(self.__height))
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError(f"{self.__height} must be > 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -48,8 +47,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
         if value <= 0:
-            raise ValueError(f"{self.__x} must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -58,6 +59,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
         if value <= 0:
-            raise ValueError(f"{self.__y} must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
