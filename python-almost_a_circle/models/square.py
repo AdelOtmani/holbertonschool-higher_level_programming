d061166aff0be02839eea6fde1916326692ee2af
AdelOtmani <5023@holbertonschool.com>
2022-10-06 14:48:11 +0200

    square's class (exercice 12)

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,29 @@ class Square(Rectangle):
     def __str__(self):
         return (f"[Square] ({self.id}) {self.x}/{self.y}"
                 f" - {self.size}")
+
+    def update(self, *args, **kwargs):
+        """adding the public methodthat assigns an argument to each attribute:
+    1st argument should be the id attribute
+    2nd argument should be the width attribute
+    3rd argument should be the height attribute
+    4th argument should be the x attribute
+    5th argument should be the y attribute
+        """
+        a = 0
+        if args:
+            for arg in args:
+                if a == 0:
+                    self.id = arg
+                if a == 1:
+                    self.width = arg
+                if a == 2:
+                    self.height = arg
+                if a == 3:
+                    self.x = arg
+                if a == 4:
+                    self.y = arg
+                a += 1
+        else:
+            for arg in kwargs:
+                setattr(self, arg, kwargs.get(arg))
