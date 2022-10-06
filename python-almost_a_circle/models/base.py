db1453c666728a83ea2df5aded0f0396a696fedf
AdelOtmani <5023@holbertonschool.com>
2022-10-06 15:14:37 +0200

    static method exo 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,6 @@ class Base:
         """returns the JSON string representation of list_dictionaries
 
         """
-        if not list_dictionaries:
+        if list_dictionaries:
             return []
         return json.dumps(list_dictionaries)
