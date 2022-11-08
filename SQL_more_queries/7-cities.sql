33074f6917eb354d82b4f6e2f70eb552252897fe
AdelOtmani <5023@holbertonschool.com>
2022-11-08 16:32:42 +0100

    exo 7 rearangement primary and foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,3 @@
 -- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT, states_id INT FOREIGN KEY(states_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT NOT NULL UNIQUE AUTO_INCREMENT, states_id INT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(states_id) REFERENCES states(id));
