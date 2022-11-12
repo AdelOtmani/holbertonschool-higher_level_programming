285a8c64dacb7524a1a8d94ded4ba4b417a6aaea
AdelOtmani <5023@holbertonschool.com>
2022-11-12 20:20:10 +0100

    module documented

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
+""" Class State, that contains the class definition of a State and an instance
 """
-Class State, that contains the class definition of a State and an instance
-"""
-
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    inherits from Base
-    """
+    """ inherits from Base """
 
     __tablename__ = "states"
     id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
