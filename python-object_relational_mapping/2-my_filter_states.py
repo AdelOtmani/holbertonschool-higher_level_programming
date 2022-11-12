d1c74514fb1493d13a4d73b1da2bd3b1eeba09f7
AdelOtmani <5023@holbertonschool.com>
2022-11-12 19:26:20 +0100

    exo 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument and displays all values of a table where name
+matches the argument.
+"""
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db = sys.argv[3]
+    mysql_name = sys.argv[4]
+    con = MySQLdb.connect(host="localhost", port=3306, user=mysql_user,
+                          passwd=mysql_passwd, db=mysql_db)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".
+                format(mysql_name))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == mysql_name:
+            print(row)
+    cur.close()
+    con.close()
