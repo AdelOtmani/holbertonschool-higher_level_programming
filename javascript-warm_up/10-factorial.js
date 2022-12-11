ce5e036f537da52edddf32a0e0821f6fdc033c7e
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-11 23:53:14 +0100

    vs studio blank line

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 function factorial (number) {
-    if (isNaN(number)) {
-      return 1;
-    }
-    if (number < 0) {
-      return -1;
-    }
-    if (number === 0) {
-      return 1;
-    } else {
-      return number * factorial(number - 1);
-    }
+  if (isNaN(number)) {
+    return 1;
   }
-  console.log(factorial(process.argv[2]));
+  if (number < 0) {
+    return -1;
+  }
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+console.log(factorial(process.argv[2]));
