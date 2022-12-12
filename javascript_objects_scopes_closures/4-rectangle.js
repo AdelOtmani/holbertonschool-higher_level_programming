9bcfb9ddb8f95f15831d2c15df319f28f1ccf280
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-12 01:59:04 +0100

    Task 4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -14,13 +14,11 @@ class Rectangle {
   }
 
   rotate () {
-    this.height = this.width;
-    this.width = this.height;
+    [this.height, this.width] = [this.width, this.height];
   }
 
   double () {
-    this.height = this.width * 2;
-    this.width = this.height * 2;
+    [this.height, this.width] = [this.height * 2, this.width * 2];
   }
 }
 module.exports = Rectangle;
