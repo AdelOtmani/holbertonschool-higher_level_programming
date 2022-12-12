4282c53825a4579f1cb2e997789c56745d764396
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-12 01:27:42 +0100

    Task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -5,9 +5,8 @@ class Rectangle {
       this.width = w;
       this.height = h;
     }
-    print () {
-      for (let i = 0; i < this.height; i++)
-        console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
