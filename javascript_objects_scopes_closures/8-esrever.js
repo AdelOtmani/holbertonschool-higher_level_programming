75b08d60eda86a59ae204f8ebe86dc1db00d4066
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-12 03:30:20 +0100

    Task 8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const l = [];
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     l.push(list[i]);
   }
   return (l);
