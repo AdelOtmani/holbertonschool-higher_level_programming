9730c7a1eea8b9f2647d1f40c58a1f4c32c089c2
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-15 14:20:13 +0100

    task4 output not url time

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,11 @@ request.get(process.argv[2], function (error, rep, num) {
   }
   const data = JSON.parse(num);
   let count = 0;
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count = count + 1;
+  for (const elem of data.results) {
+    for (const people of elem.characters) {
+      if (people.includes(18)) {
+        count = count + 1;
+      }
     }
   }
   console.log(count);
