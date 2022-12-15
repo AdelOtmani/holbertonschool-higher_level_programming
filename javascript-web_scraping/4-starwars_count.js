01093c967d1437d60309165096c3eeb18db8e755
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-15 14:14:36 +0100

    task4 output

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let count = 0;
 request.get(process.argv[2], function (error, rep, num) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(num);
+  let count = 0;
   for (let i = 0; data.results[i] !== undefined; i++) {
     if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
       count = count + 1;
