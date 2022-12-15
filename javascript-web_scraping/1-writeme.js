1d3610751675f9835073cc2b6193301fbba932a5
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-15 13:48:59 +0100

    task1 output

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.appendFile(process.argv[2], process.argv[3], function (error, content) {
-  console.log(error || content);
+  if (error) {
+    console.log(error);
+  }
+  console.log(content);
 });
